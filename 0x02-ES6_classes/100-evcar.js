0c99d0d0dc7799f66fdca1829e3782c06cf5082f
Linda-Njau <lindanjau21@gmail.com>
2023-06-30 00:58:47 +0300

    eslint correction

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car.js'; // eslint-disable-line
 
 export default class Evcar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this._range = range; //eslint-disable-line
   }
 
-  cloneCar() {
+  cloneCar() { // eslint-disable-line
     return new Car();
   }
 }
