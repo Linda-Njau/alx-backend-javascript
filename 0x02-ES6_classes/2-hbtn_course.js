3edcff064e2b74c4fb43dcf4dbfa994c4cf21ada
Linda-Njau <lindanjau21@gmail.com>
2023-06-29 18:39:54 +0300

    task 2 added

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -12,44 +12,44 @@ export default class HolbertonCourse {
     if (!students.every((student) => typeof student === 'string')) {
       throw TypeError('students must be an array of strings');
     }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this._name = name;  //eslint-disable-line 
+    this._length = length; //eslint-disable-line 
+    this._students = students; //eslint-disable-line 
   }
 
   get name() {
-    return this._name;
+    return this._name; //eslint-disable-line 
   }
 
   set name(value) {
     if (typeof value !== 'string') {
       throw TypeError('Name must be a string');
     }
-    this._name = value;
+    this._name = value; //eslint-disable-line 
   }
 
   get length() {
-    return this._length;
+    return this._length; //eslint-disable-line 
   }
 
   set length(value) {
     if (typeof value !== 'number') {
       throw TypeError('Length must be a number');
     }
-    this._length = value;
+    this._length = value; //eslint-disable-line 
   }
 
   get students() {
-    return this._students;
+    return this._students; //eslint-disable-line 
   }
 
   set students(value) {
     if (!Array.isArray(value)) {
       throw TypeError('Students must be an array');
     }
-    if (!value.every((students) => typeof value !== 'string')) {
+    if (!value.every((students) => typeof value !== 'string')) { //eslint-disable-line 
       throw TypeError('students must be an array of strings');
     }
-    this._students = value;
+    this._students = value; //eslint-disable-line 
   }
 }
