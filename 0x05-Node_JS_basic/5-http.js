162e9432e609490583a86e27c84db6f9089a4fab
Linda-Njau <lindanjau21@gmail.com>
2023-08-22 14:41:55 +0300

    task 5 correction

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -15,8 +15,8 @@ const app = http.createServer((req, res) => {
       .then((data) => {
         res.write('This is the list of our students\n');
         res.write(`Number of students: ${data.students.length}\n`);
-        res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(',')}\n`);
-        res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(',')}\n`);
+        res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+        res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}\n`);
         res.end();
       })
       .catch((err) => res.end(err.message));
