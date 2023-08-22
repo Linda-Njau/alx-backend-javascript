8b89816841bb00e217bab543b5ecffb676c4f540
Linda-Njau <lindanjau21@gmail.com>
2023-08-22 15:16:06 +0300

    Correction on catch block

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -18,8 +18,7 @@ const app = http.createServer((req, res) => {
         res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
         res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}\n`);
         res.end();
-      })
-      .catch((err) => res.end(err.message));
+      }).catch((err) => res.end(err.message));
   }
 });
 
