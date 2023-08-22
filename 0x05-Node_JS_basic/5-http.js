f002ba9da266af2dd63b726de0a77215dab8ba24
Linda-Njau <lindanjau21@gmail.com>
2023-08-22 15:23:44 +0300

    Change to print statement

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -10,15 +10,13 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const filePath = process.argv[2] || 'database.csv';
-    students(filePath)
-      .then((data) => {
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${data.students.length}\n`);
-        res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-        res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}\n`);
-        res.end();
-      }).catch((err) => res.end(err.message));
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}\n`);
+      res.end();
+    }).catch((err) => res.end(err.message));
   }
 });
 
