0824430d2a123720076c6d35b4f700b3a88aa19c
Linda-Njau <lindanjau21@gmail.com>
2023-08-23 21:29:10 +0300

    Added expect from chai to the testsuite task 2

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,32 +1,33 @@
+const chai = require('chai');
 const assert = require('assert');
-const calculateNumber = require('./1-calcul.js');
+const calculateNumber = require('./1-calcul_chai.js');
 
 describe('calculateNumber', () => {
     it('should calculate a sum', () => {
-        expect(calculateNumber('SUM', 2, 3)).to.equal(5);
-        expect(calculateNumber('SUM', 2.5, 2.7)).to.equal(6);
-        expect(calculateNumber('SUM', -3.2, 2)).to.equal(-1);
-        expect(calculateNumber('SUM', 2.2, 1.2)).to.equal(3);
+        assert.strictEqual(calculateNumber('SUM', 2, 3), 5);
+        assert.strictEqual(calculateNumber('SUM', 2.5, 2.7), 6);
+        assert.strictEqual(calculateNumber('SUM', -3.2, 2), -1);
+        assert.strictEqual(calculateNumber('SUM', 2.2, 1.2), 3);
     });
     it('should calculate substraction', () => {
-        expect(calculateNumber('SUBTRACT', 2, 3)).to.equal(-1);
-        expect(calculateNumber('SUBTRACT', 2, 2)).to.equal(0);
-        expect(calculateNumber('SUBTRACT', 4, 2)).to.equal(2);
-        expect(calculateNumber('SUBTRACT', -2, -2)).to.equal(0);
-        expect(calculateNumber('SUBTRACT', -2.7, 1)).to.equal(-4);
-        expect(calculateNumber('SUBTRACT', -2.3, -1)).to.equal(-1);
-        expect(calculateNumber('SUBTRACT', -2.4, -1.7)).to.equal(0);
+        assert.strictEqual(calculateNumber('SUBTRACT', 2, 3), -1);
+        assert.strictEqual(calculateNumber('SUBTRACT', 2, 2), 0);
+        assert.strictEqual(calculateNumber('SUBTRACT', 4, 2), 2);
+        assert.strictEqual(calculateNumber('SUBTRACT', -2, -2), 0);
+        assert.strictEqual(calculateNumber('SUBTRACT', -2.7, 1), -4);
+        assert.strictEqual(calculateNumber('SUBTRACT', -2.3, -1), -1);
+        assert.strictEqual(calculateNumber('SUBTRACT', -2.4, -1.7), 0);
     });
     it('should calculate division', () => {
-        expect(calculateNumber('DIVIDE', 2, 2)).to.equal(1);
-        expect(calculateNumber('DIVIDE', -2, -2)).to.equal(1);
-        expect(calculateNumber('DIVIDE', -2, 2)).to.equal(-1);
-        expect(calculateNumber('DIVIDE', 1.7, 2)).to.equal(1);
-        expect(calculateNumber('DIVIDE', 2.3, 2.3)).to.equal(1);
-        expect(calculateNumber('DIVIDE', -2.4, -1.7)).to.equal(1);
+        assert.strictEqual(calculateNumber('DIVIDE', 2, 2), 1);
+        assert.strictEqual(calculateNumber('DIVIDE', -2, -2), 1);
+        assert.strictEqual(calculateNumber('DIVIDE', -2, 2), -1);
+        assert.strictEqual(calculateNumber('DIVIDE', 1.7, 2), 1);
+        assert.strictEqual(calculateNumber('DIVIDE', 2.3, 2.3), 1);
+        assert.strictEqual(calculateNumber('DIVIDE', -2.4, -1.7), 1);
     });
     it ('should return "Error" when b is rounded to 0', () => {
-        expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error');
-        expect(calculateNumber('DIVIDE', 2, 0.3)).to.equal('Error');
+        assert.strictEqual(calculateNumber('DIVIDE', 2, 0), 'Error');
+        assert.strictEqual(calculateNumber('DIVIDE', 2, 0.3), 'Error');
     });
 })
